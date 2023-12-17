75e989b48d4deee770435d423f08aacb44389abb
Courtcircuits <tristan-mihai.radulescu@etu.umontpellier.fr>
2023-12-17 11:20:01 +0100

    rose pine midnight

--- lua/rose-pine/palette.lua
+++ lua/rose-pine/palette.lua
@@ -22,6 +22,27 @@ local variants = {
 		highlight_high = '#524f67',
 		none = 'NONE',
 	},
+	midnight = {
+		---@deprecated for backwards compatibility
+		_experimental_nc = '#16141f',
+		nc = '#16141f',
+		base = '#191724',
+		surface = '#1f1d2e',
+		overlay = '#16141f',
+		muted = '#6e6a86',
+		subtle = '#908caa',
+		text = '#e0def4',
+		love = '#eb6f92',
+		gold = '#f6c177',
+		rose = '#ebbcba',
+		pine = '#31748f',
+		foam = '#9ccfd8',
+		iris = '#c4a7e7',
+		highlight_low = '#21202e',
+		highlight_med = '#403d52',
+		highlight_high = '#524f67',
+		none = 'NONE',
+	},
 	moon = {
 		---@deprecated for backwards compatibility
 		_experimental_nc = '#1f1d30',
